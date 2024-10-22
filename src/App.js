9efa892c03366eb274ea7bc103ff393d94579620
Krishna Kakadiya <krishna.nexios@gmail.com>
2024-10-22 17:18:47 +0530

    make login design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/auth/login/Login';
 import DrawerBox from './components/drawer/Index';
 
+
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
-    <div className="App">
-      <DrawerBox />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DrawerBox />} />
+        
+      </Routes>
+    </Router>
+
   );
 }
 
